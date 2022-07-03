0801cccdf4455c0e6410c9e333738e5070ca03c3
ergenekonyigit <yigitergenekon@gmail.com>
2022-07-04 01:35:12 +0300

    add github url

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,6 +7,17 @@ export default function Home() {
   return (
     <div class={tw`grid h-screen place-items-center`}>
       <GaltonBoard />
+      <div class={tw`text(center)`}>
+        <div class={tw`font-bold text(xl blue-400)`}>Galton Board</div>
+        <a
+          class={tw`text(xs gray-400)`}
+          target="_blank"
+          alt="GitHub URL"
+          href="https://github.com/ergenekonyigit/preact-galton-board"
+        >
+          github.com/ergenekonyigit/preact-galton-board
+        </a>
+      </div>
     </div>
   );
 }
